430518fe6182084fe422b09648f2e7ed69cc3a71
inovacaomab <166883848+inovacaomab@users.noreply.github.com>
2024-06-03 11:46:28 -0300

    Update script.js
    Signed-off-by: inovacaomab <166883848+inovacaomab@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var phoneNumberInput = document.querySelector('#phoneNumber');
-    window.intlTelInput(phoneNumberInput, {
-        initialCountry: "br",
-        preferredCountries: ['br', 'us'],
-        separateDialCode: true,
-        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
-    });
+  var phoneNumberInput = document.querySelector('#phoneNumber');
+  window.intlTelInput(phoneNumberInput, {
+    initialCountry: "br",
+    preferredCountries: ['br', 'us'],
+    separateDialCode: true,
+    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/js/utils.js"
+  });
 
-    window.openWhatsApp = function() {
-        var message = encodeURIComponent(document.querySelector('#messageText').value);
-        var phoneNumber = phoneNumberInput.value.replace(/\D/g,'');
-        if (phoneNumber) {
-            var url = `https://wa.me/${phoneNumber}`;
-            if (message) {
-                url += `?text=${message}`;
-            }
-            window.open(url, '_blank');
-        } else {
-            alert('Please enter a phone number.');
-        }
+  window.openWhatsApp = function() {
+    var message = encodeURIComponent(document.querySelector('#messageText').value);
+    var phoneNumber = phoneNumberInput.value.replace(/\D/g,'');
+    if (phoneNumber) {
+      var url = `https://wa.me/${phoneNumber}`;
+      if (message) {
+        url += `?text=${message}`;
+      }
+      window.open(url, '_blank');
+    } else {
+      alert('Please enter a phone number.');
     }
+  }
 });
