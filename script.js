ad1f0b883a4d9da4639a0f45ae8f11ab9eb7c74f
Moacir ADM <166883848+inovacaomab@users.noreply.github.com>
2024-07-16 15:48:05 -0300

    Update script.js
    correção do erro na escolha do país.
    
    Signed-off-by: Moacir ADM <166883848+inovacaomab@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var phoneNumberInput = document.querySelector('#phoneNumber');
-  window.intlTelInput(phoneNumberInput, {
-    initialCountry: "br",
-    preferredCountries: ['br', 'us'],
-    separateDialCode: true,
-    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
-  });
+    var phoneNumberInput = document.querySelector('#phoneNumber');
+    var iti = window.intlTelInput(phoneNumberInput, {
+        initialCountry: "br",
+        preferredCountries: ['br', 'us'],
+        separateDialCode: true,
+        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
+    });
 
-  window.openWhatsApp = function() {
-    var message = encodeURIComponent(document.querySelector('#messageText').value);
-    var phoneNumber = phoneNumberInput.value.replace(/\D/g,'');
-    if (phoneNumber) {
-      var url = `https://wa.me/${phoneNumber}`;
-      if (message) {
-        url += `?text=${message}`;
-      }
-      window.open(url, '_blank');
-    } else {
-      alert('Please enter a phone number.');
-    }
-  }
+    window.openWhatsApp = function() {
+        var message = encodeURIComponent(document.querySelector('#messageText').value);
+        var phoneNumber = phoneNumberInput.value.replace(/\D/g,'');
+        if (phoneNumber) {
+            var countryData = iti.getSelectedCountryData();
+            var dialCode = countryData.dialCode;
+            phoneNumber = '+' + dialCode + phoneNumber;
+            var url = `https://wa.me/${phoneNumber}`;
+            if (message) {
+                url += `?text=${message}`;
+            }
+            window.open(url, '_blank');
+        } else {
+            alert('Por favor, insira um número de telefone.');
+        }
+    }
 });
