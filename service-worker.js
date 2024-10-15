fa2a1f6dc43d44789aa96f471abc5294ef424524
Moacir ADM <166883848+inovacaomab@users.noreply.github.com>
2024-10-15 10:43:56 -0300

    Add files via upload
    Signed-off-by: Moacir ADM <166883848+inovacaomab@users.noreply.github.com>

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,45 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('static-v1').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/script.js',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png',
-                '/iconeprincipal.jpg',
-                '/qricon.jpg'
-            ]);
-        })
-    );
+const CACHE_NAME = 'whats-go-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/iconeprincipal.jpg',
+  '/qricon.jpg',
+  '/iconezinho.ico'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
